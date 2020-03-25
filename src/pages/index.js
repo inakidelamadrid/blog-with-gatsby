c53db3ae3e93344f56d5e43566cd585be106f66c
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2020-03-24 22:04:39 -0600

    show markdown posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,38 @@
 import React from "react"
-import range from "lodash/range"
-import "../styles/main.scss"
-import IndexBlogEntry from "../components/IndexBlogEntry"
-import Container, { PrimaryContent } from "../components/Container"
-import Footer from "../components/Footer"
-import Header from "../components/Header"
+import map from "lodash/map"
+import { StaticQuery, graphql } from "gatsby"
+import SimpleHeader from "../components/SimpleHeader"
 
-export default () => (
-  <Container>
-    <PrimaryContent>
-      <Header name="BorealSummit" />
-      {range(5).map(n => (
-        <IndexBlogEntry
-          author="Paul Laros"
-          content={
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
-            </p>
+const Layout = ({ data }) => {
+  //console.log("%c GraphQL data", "font-weight: bold; color: blue;", data)
+  const { edges } = data.allMarkdownRemark
+  const posts = map(edges, 'node.frontmatter')
+
+  //console.log("%c Posts", "font-weight: bold; color: blue;", posts)
+  return (
+    <div>
+      <SimpleHeader />
+      <ul>
+        {map(posts, post => <li key={post.path}>{post.title}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query HomePageQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
           }
-          key={n}
-          tags={[{ title: "HTML5", href: "tag.html" }]}
-          title="Why should designers learn how to code"
-          linkTo="/post"
-        />
-      ))}
-      <Footer />
-    </PrimaryContent>
-  </Container>
-)
+        }
+      }
+    }
+  }
+`
+
+export default Layout
