9aae0743b382ae77032a9dcbd4a23e1292163ba9
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2020-03-24 22:48:13 -0600

    add dynamic markdown pages

---
+++ gatsby-node.js
@@ -0,0 +1,37 @@
+const path = require("path")
+
+exports.createPages = async({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve("src/templates/blogPost.js")
+
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  console.log("Result", result)
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const path = node.frontmatter.path
+    createPage({
+      path,
+      component: blogPostTemplate,
+      context: {
+        pathSlug: path,
+      },
+    })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
 import React from "react"
 import map from "lodash/map"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import SimpleHeader from "../components/SimpleHeader"
 
 const Layout = ({ data }) => {
   //console.log("%c GraphQL data", "font-weight: bold; color: blue;", data)
   const { edges } = data.allMarkdownRemark
-  const posts = map(edges, 'node.frontmatter')
+  const posts = map(edges, "node.frontmatter")
 
   //console.log("%c Posts", "font-weight: bold; color: blue;", posts)
   return (
     <div>
       <SimpleHeader />
       <ul>
-        {map(posts, post => <li key={post.path}>{post.title}</li>)}
+        {map(posts, post => (
+          <li key={post.path}>
+            <Link to={post.path}>{post.title}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   )
@@ -21,9 +25,7 @@ const Layout = ({ data }) => {
 
 export const query = graphql`
   query HomePageQuery {
-    allMarkdownRemark(
-      sort: {order: DESC, fields: [frontmatter___date]}
-    ) {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
       edges {
         node {
           frontmatter {
