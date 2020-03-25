e13dade9e7cf94fc54886828b14693b29c63d3bd
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2020-03-24 18:12:53 -0600

    fix footer look

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,25 +6,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const Footer = props => {
   return (
     <footer className={styles.footer}>
-      <p>© Von 2013. All right reserved.</p>
+      <p>
+        © Ignacio De La Madrid {new Date().getFullYear()}. All rights reserved.
+      </p>
       <div className={styles.socialsFooter}>
         <a className="btn btn-twitter" href="/" title="Twitter">
-          <FontAwesomeIcon icon={['fab', 'twitter']}/>
+          <FontAwesomeIcon icon={["fab", "twitter"]} />
         </a>
         <a className="btn btn-linkedin" href="/" title="LinkedIn">
-          <i className="fa fa-linkedin"></i>
-        </a>
-        <a className="btn btn-google-plus" href="/" title="Google+">
-          <i className="fa fa-google-plus"></i>
-        </a>
-        <a className="btn btn-instagram" href="/" title="Instagram">
-          <i className="fa fa-instagram"></i>
+          <FontAwesomeIcon icon={["fab", "linkedin"]} />
         </a>
         <a className="btn btn-github" href="/" title="GitHub">
-          <i className="fa fa-github"></i>
-        </a>
-        <a className="btn btn-bitbucket" href="/" title="Bitbucket">
-          <i className="fa fa-bitbucket"></i>
+          <FontAwesomeIcon icon={["fab", "github"]} />
         </a>
       </div>
     </footer>
