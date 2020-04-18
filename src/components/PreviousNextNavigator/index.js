7fc2fb2d1dc57bcd03e5b0ba87742cf3a34aab9b
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2020-04-18 12:26:39 -0500

    Style the navigator

--- src/components/PreviousNextNavigator/index.js
+++ src/components/PreviousNextNavigator/index.js
@@ -1,24 +1,34 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "gatsby"
+import styles from "./styles.module.scss"
+import classNames from "classnames"
 
 const Previous = ({ path }) => (
-  <Link to={path}>
-    <FontAwesomeIcon icon={"arrow-left"} />
-    Previous
-  </Link>
+  <div className={styles.children}>
+    <Link to={path}>
+      <FontAwesomeIcon icon={"arrow-left"} />
+      Previous
+    </Link>
+  </div>
 )
 const Next = ({ path }) => (
-  <Link to={path}>
-    Next
-    <FontAwesomeIcon icon={"arrow-right"} />
-  </Link>
+  <div className={classNames(styles.children, styles.textRight)}>
+    <Link to={path}>
+      Next
+      <FontAwesomeIcon icon={"arrow-right"} />
+    </Link>
+  </div>
 )
 
 const PreviousNextNavigator = ({ prev, next }) => {
   return (
-    <div>
-      {prev && <Previous path={prev.frontmatter.path} />}
+    <div className={styles.navigator}>
+      {prev ? (
+        <Previous path={prev.frontmatter.path} />
+      ) : (
+        <div className={styles.children}></div>
+      )}
       {next && <Next path={next.frontmatter.path} />}
     </div>
   )
